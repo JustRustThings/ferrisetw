1c4f9d1d607194c992d1cd38fed4cdb7b01a49c0
Axel Rousselot <axel.rousselto@harfanglab.fr>
2025-11-20 16:32:20 +0100

    fix(parser): Add support for TDH InTypeReversedCountedString parsing

--- src/native/tdh_types.rs
+++ src/native/tdh_types.rs
@@ -196,6 +196,9 @@ pub enum TdhInType {
     InTypeHexInt32,
     InTypeHexInt64,
     InTypeCountedString = 300,
+    InTypeCountedAnsiString,
+    InTypeReversedCountedString,
+    InTypeReversedCountedAnsiString,
 }
 
 /// Represent a TDH_OUT_TYPE
